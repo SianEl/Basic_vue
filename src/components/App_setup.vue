ae5672c43d772e414e147ac4d07736bf72b22464
PP08793 <11st.pp08793@partner.sk.com>
2022-12-08 10:24:14 +0900

    Vue3 연습

---
+++ src/components/App_setup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>
+      {{ counter }}
+    </p>
+    <p>
+      {{ message }}
+    </p>
+    <button @click="increment">Click!</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  props: {
+    title: String,
+  },
+  setup(props, context) {
+    console.log(props.title); // undefined
+    console.log(props); //Proxy {title:undefined}
+
+    console.log(context.attrs);
+    console.log(context.emit);
+    console.log(context.expose);
+    console.log(context.slots);
+
+    const counter = ref(0);
+    const message = ref('Hello Vue3');
+    const increment = () => {
+      counter.value++;
+    };
+    return {
+      counter,
+      message,
+      increment,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/App_template.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>문자열: {{ message }}</p>
+    <!-- message가 바인딩 되어 !가 계속 추가되는 것을 확인 -->
+
+    <p v-once>문자열: {{ message }}</p>
+    <!-- 일회성으로 하는 v-once를 사용하여 message가 갱신되지 않는것을 확인-->
+    <button v-on:click="addMessage">Click!</button>
+    <hr />
+    <p>{{ rowHtml }}</p>
+    <!--일반 문자열-->
+
+    <p v-html="rowHtml"></p>
+    <!--HTML로 치환-->
+    <hr />
+    <h2>속성 바인딩</h2>
+    <div v-bind:title="dynamicTitle">마우스를 올려보세요</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('안녕하세요');
+    const addMessage = () => {
+      message.value = message.value + '!';
+    };
+    const rowHtml = ref('<strong>안녕하세요</strong>');
+    const dynamicTitle = ref('안녕하세요!!!');
+    return {
+      message,
+      rowHtml,
+      dynamicTitle,
+      addMessage,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
