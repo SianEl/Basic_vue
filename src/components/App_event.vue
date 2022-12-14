375a5be8ecef74167cac888163956016602fa957
PP08793 <11st.pp08793@partner.sk.com>
2022-12-14 15:59:45 +0900

    event practice

---
+++ src/components/App_event.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      <button @click="counter++">counter {{ counter }}</button>
+    </div>
+    <hr />
+    <div>
+      <button @click="printEventInfo">printEventInfo</button>
+    </div>
+    <hr />
+    <div>
+      <button @click="printEventInfo2('hello world', $event)">
+        inline event handler
+      </button>
+    </div>
+    <hr />
+    <input type="text" @keyup="onKeyUpHandler" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const counter = ref(0);
+    const printEventInfo = event => {
+      console.log(event.target);
+      console.log(event.target.tagName);
+    };
+
+    const printEventInfo2 = (message, event) => {
+      console.log('message: ' + message);
+      console.log(event.target);
+      console.log(event.target.tagName);
+    };
+
+    const onKeyUpHandler = event => {
+      console.log('event.key: ', event.key);
+    };
+
+    return {
+      counter,
+      printEventInfo,
+      printEventInfo2,
+      onKeyUpHandler,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
