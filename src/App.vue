82713d4f6e9d45b01afaec927391b2b218a3d840
PP08793 <11st.pp08793@partner.sk.com>
2022-12-19 08:13:18 +0900

    lint 적용

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <div></div>
+  <div>
+    <div>
+      <h1>양방향 바인딩</h1>
+      <input type="text" v-model="inputValue" />
+      <button @click="valueCheck">확인</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   setup() {
-    return {};
+    const inputValue = ref('');
+
+    const valueCheck = () => {
+      console.log(inputValue.value);
+    };
+
+    return {
+      inputValue,
+      valueCheck,
+    };
   },
 };
 </script>
