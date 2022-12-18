e54fe573f91f22f8b6aec143692c561581a68e8d
Cyan <2wnaud@gmail.com>
2022-12-18 16:27:49 +0900

    컴포넌트 기초

---
+++ src/components/lectures/17/App_component.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <TheNav></TheNav>
+    <TheView></TheView>
+    <!-- <AppCard></AppCard> -->
+  </div>
+</template>
+
+<script>
+import TheNav from './components/lectures/17/TheNav.vue';
+import TheView from './components/lectures/17/TheView.vue';
+export default {
+  components: {
+    TheNav,
+    TheView,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
-
-createApp(App).mount('#app');
 import './assets/main.css';
+// import AppCard from '@/components/lectures/17/AppCard.vue';
 
+const app = createApp(App);
+
+// app.component('AppCard', AppCard);
+
+app.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
