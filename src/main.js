c4e6dac668a7da0c56a47b20927a631ded433249
Cyan <2wnaud@gmail.com>
2022-12-18 11:00:11 +0900

    bootstrap5 활용

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+createApp(App).mount('#app');
 import './assets/main.css';
 
-createApp(App).mount('#app');
+import 'bootstrap/dist/js/bootstrap.js';
