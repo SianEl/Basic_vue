f01987f8a15b78876dd89e61e267795aecd5cc85
PP08793 <11st.pp08793@partner.sk.com>
2022-12-20 12:45:18 +0900

    postItem 등록

---
+++ src/views/posts/PostListView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>게시글 목록</h2>
+    <hr class="my-4" />
+    <div class="row g-3">
+      <div v-for="post in posts" :key="post.id" class="col-3">
+        <PostItem
+          :title="post.id"
+          :content="post.content"
+          :created-at="post.createdAt"
+        ></PostItem>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import PostItem from '@/components/practice/posts/PostItem.vue';
+import { ref } from 'vue';
+import { getPosts } from '@/api/posts';
+const posts = ref([]);
+
+const fetchPosts = () => {
+  posts.value = getPosts();
+};
+fetchPosts();
+</script>
+
+<style lang="scss" scoped></style>
